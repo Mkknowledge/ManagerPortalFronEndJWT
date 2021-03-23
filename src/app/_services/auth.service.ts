b09d155c96b85341902be69807e807ac8a7f47c0
Mkknowledge <kmayur2124@gmail.com>
2021-03-23 23:19:06 +0530

    Service to send signup, login HTTP POST resquests to back-end

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'http://localhost:8080/api/auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(credentials): Observable<any> {
+    return this.http.post(AUTH_API + 'signin', {
+      username: credentials.username,
+      password: credentials.password
+    }, httpOptions);
+  }
+
+  register(manager): Observable<any> {
+    return this.http.post(AUTH_API + 'signup', {
+      username: manager.username,
+      email: manager.email,
+      firstname: manager.firstname,
+      lastname:manager.lastname,
+      password: manager.password,
+      address: manager.address,
+      dob: manager.dob,
+      company: manager.company
+    }, httpOptions);
+  }
+
 }
