3af694560358f936f0349af894e661f14b8a4c4e
Mkknowledge <kmayur2124@gmail.com>
2021-03-23 23:09:14 +0530

    Generated component and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardManagerComponent } from './board-manager/board-manager.component';
+import { BoardEmployeeComponent } from './board-employee/board-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardManagerComponent,
+    BoardEmployeeComponent
   ],
   imports: [
     BrowserModule,
